d9a7fba33aa455478905b4dd7a571b3a6c067fb5
Md.Parvez <parvez@localhost.localdomain>
2023-03-23 18:46:44 +0600

    first commit

---
+++ employee/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('registration/', views.registration, name='registration'),
+    path('emp_login/', views.emp_login, name='emp_login'),
+    path('emp_home/', views.emp_home, name='emp_home'),
+    path('profile/', views.profile, name='profile'),
+    path('logout/', views.Logout, name='logout'),
+    path('admin_login/', views.admin_login, name='admin_login'),
+    path('my_experience/', views.my_experience, name='my_experience'),
+    path('edit_myexperience/', views.edit_myexperience, name='edit_myexperience'),
+    path('my_education/', views.my_education, name='my_education'),
+    path('edit_myeducation/', views.edit_myeducation, name='edit_myeducation'),
+    path('change_password/', views.change_password, name='change_password'),
+    path('admin_home/', views.admin_home, name='admin_home'),
+    path('change_passwordadmin/', views.change_passwordadmin, name='change_passwordadmin'),
+    path('all_employee/', views.all_employee, name='all_employee'),
+    path('delete_employee/<int:pid>', views.delete_employee, name='delete_employee'),
+    path('edit_profile/<int:pid>', views.edit_profile, name='edit_profile'),
+    path('edit_education/<int:pid>', views.edit_education, name='edit_education'),
+    path('edit_experience/<int:pid>', views.edit_experience, name='edit_experience'),
+]
